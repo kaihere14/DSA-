8a82f4fed8ed5fcd434665024e37c8ed4a8df431
Arman Thakur <yashpalsharma9950@gmail.com>
2025-05-31 10:10:39 +0530

    Time: 3 ms (72.57%) | Memory: 43.6 MB (52.35%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,8 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String onlyletters = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int start =0;int end =onlyletters.length()-1; 
-
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        
+        String onlyletters = sb.toString();
+        int start = 0, end = onlyletters.length() - 1;
         while(start<=end){
             if(onlyletters.charAt(start)!=onlyletters.charAt(end)){
                 return false;
