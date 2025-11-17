c0387d144195cac8a84e282b0c547bdb638e66c8
Arman Thakur <yashpalsharma9950@gmail.com>
2025-11-18 00:16:40 +0530

    Time: 1 ms (59.80%) | Memory: 46.8 MB (8.15%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,39 +3,14 @@ import java.util.HashSet;
 
 class Solution {
     public int removeDuplicates(int[] arr) {
-       int l = 0;
-        HashSet<Integer> duplicate = new HashSet<>();
-
-        for(int i =0;i<arr.length;i++){
-            duplicate.add(arr[i]);
-        }
-        int newArr[]= new int[duplicate.size()];
-        for(int element : duplicate){
-            newArr[l++] = element;
-        }
-
-
-        for (int i = 0; i < newArr.length - 1; i++) {
-            int minIndex = i;
-
-
-            for (int j = i + 1; j < newArr.length; j++) {
-                if (newArr[j] < newArr[minIndex]) {
-                    minIndex = j;
-                }
+       int i =0;
+        for(int k =1;k<arr.length;k++){
+            if(arr[i]!=arr[k]){
+                i++;
+                arr[i]= arr[k];
             }
-
-
-            int temp = newArr[minIndex];
-            newArr[minIndex] = newArr[i];
-            newArr[i] = temp;
         }
-
-        for(int i =0;i< newArr.length;i++){
-           arr[i]= newArr[i];
-        }
-
-        return duplicate.size();
+        return ++i;
 
     }
 }
