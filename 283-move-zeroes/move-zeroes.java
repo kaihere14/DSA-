601330f166587667526aebba142c802112b089ac
Arman Thakur <yashpalsharma9950@gmail.com>
2025-11-19 00:28:29 +0530

    Time: 3 ms (19.23%) | Memory: 48 MB (7.06%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,13 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        ArrayList<Integer> nonZero = new ArrayList<>();
-        for(int i = 0;i< arr.length;i++){
+       int k =0;
+        for(int i=0;i< arr.length;i++){
             if(arr[i]!=0){
-                nonZero.add(arr[i]);
+                arr[k]=arr[i];
+                k++;
             }
-        }
-        for(int i =0;i< arr.length;i++){
-            if(i<nonZero.size()){
-                arr[i]=nonZero.get(i);
-            }else{
-                arr[i]= 0;
+            if(i>=k){
+                arr[i]=0;
             }
         }
     }
