27c382c7a1834a6d8caa4316615d4c62214a3b0a
Arman Thakur <yashpalsharma9950@gmail.com>
2025-11-21 00:52:40 +0530

    Time: 1 ms (46.78%) | Memory: 63.2 MB (5.85%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -7,16 +7,10 @@ class Solution {
             temp[l++] = arr[i];
         }
         for(int i = arr.length-d-1;i>=0;i--){
-//            System.out.println(i+d+"-"+i);
             arr[i+d]=arr[i];
         }
         for(int i =0;i< temp.length;i++){
             arr[i]= temp[i];
-        }
-
-
-        for(int i = 0;i< arr.length;i++){
-            System.out.println(arr[i]);
         }
     }
 }
