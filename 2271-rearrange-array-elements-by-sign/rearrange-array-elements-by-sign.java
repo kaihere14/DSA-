97fbe8877205574c312b0ff1c98e1d44654f9702
Arman Thakur <yashpalsharma9950@gmail.com>
2025-12-04 03:00:07 -0800

    Time: 3 ms (100.00%) | Memory: 175.2 MB (33.53%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,31 +1,17 @@
 class Solution {
     public int[] rearrangeArray(int[] arr) {
-        int length = (arr.length/2);
-        int[] positive = new int[length];
-        int[] negative = new int[length];
+        int[] arr2 = new int[arr.length];
         int pos_start = 0;
-        int neg_start = 0;
-        for(int i =0;i<arr.length;i++){
+        int neg_start = 1;
+        for(int i  =0;i<arr.length;i++){
             if(arr[i]>0){
-                positive[pos_start] = arr[i];
-                pos_start++;
-                System.out.println("positive"+arr[i]);
+                arr2[pos_start]=arr[i];
+                pos_start += 2;
             }else if (arr[i]<0){
-                negative[neg_start++] = arr[i];
-                System.out.println("negative : "+arr[i]);
+                arr2[neg_start]=arr[i];
+                neg_start +=2;
             }
         }
-        pos_start =0;
-        neg_start=0;
-        for(int i =0;i<arr.length;i++){
-            if(i%2==0){
-                arr[i]=positive[pos_start++];
-            }else{
-                arr[i]= negative[neg_start++];
-            }
-        }
-
-        
-        return arr;
+        return arr2;
     }
 }
