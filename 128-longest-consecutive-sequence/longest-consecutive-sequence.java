e336e517a66bd7549f7eb9dafbc3765ed5bd6f94
Arman Thakur <yashpalsharma9950@gmail.com>
2025-12-08 22:02:42 +0530

    Time: 31 ms (63.96%) | Memory: 70.1 MB (59.88%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -2,35 +2,25 @@ class Solution {
     public static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
             int mid = left + (right - left) / 2;
-
-            // Sort left half
             mergeSort(arr, left, mid);
-
-            // Sort right half
             mergeSort(arr, mid + 1, right);
-
-            // Merge both halves
             merge(arr, left, mid, right);
         }
     }
 
-    // Function to merge two sorted halves
+
     public static void merge(int[] arr, int left, int mid, int right) {
-        // Sizes of two subarrays
         int n1 = mid - left + 1;
         int n2 = right - mid;
-
-        // Temporary arrays
         int[] L = new int[n1];
         int[] R = new int[n2];
 
-        // Copy data
         for (int i = 0; i < n1; i++)
             L[i] = arr[left + i];
         for (int j = 0; j < n2; j++)
             R[j] = arr[mid + 1 + j];
 
-        // Merge the temp arrays
+
         int i = 0, j = 0, k = left;
 
         while (i < n1 && j < n2) {
@@ -44,7 +34,7 @@ class Solution {
             k++;
         }
 
-        // Copy remaining elements
+
         while (i < n1) {
             arr[k] = L[i];
             i++;
