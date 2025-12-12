073dc9160d90a953d4fa7455cfe82abf73693e45
Arman Thakur <yashpalsharma9950@gmail.com>
2025-12-12 19:39:17 +0530

    Time: 1 ms (63.00%) | Memory: 47.4 MB (14.52%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,28 +1,19 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-        
-        for(int i =0 ;i<arr.length;i++){
-            for(int j =0;j<arr[i].length;j++){
+        int row[]= new int[arr.length];
+        int col[] = new int[arr[0].length];
+        for(int i =0;i<arr.length;i++){
+            for(int j = 0;j<arr[i].length;j++){
                 if(arr[i][j]==0){
-                    for(int l =0;l<arr.length;l++){
-                        if(arr[l][j]!=0){
-                            arr[l][j]=-999;
-                        }
-                    }
-                     for(int k =0;k<arr[i].length;k++){
-                        if(arr[i][k]!=0){
-                            arr[i][k]=-999;
-                        }
-                    }
-                    
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
 
-        
-            for(int i =0 ;i<arr.length;i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if(arr[i][j]==-999){
+        for(int i =0;i<arr.length;i++){
+            for(int j = 0;j<arr[i].length;j++){
+                if(row[i]==1 || col[j]==1){
                     arr[i][j]=0;
                 }
             }
