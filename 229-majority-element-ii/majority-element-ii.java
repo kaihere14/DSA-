2d4b714f0f54e690e4abcacf58ebbc5661c79005
Arman Thakur <yashpalsharma9950@gmail.com>
2025-12-17 01:14:40 +0530

    Time: 21 ms (7.17%) | Memory: 48.9 MB (99.59%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,19 +1,24 @@
 class Solution {
     public List<Integer> majorityElement(int[] arr) {
-        int minLen = arr.length/3;
-        int count = 0;
+         int minLen = arr.length/3;
+
+        HashMap<Integer,Integer> counter = new HashMap();
+
         List<Integer> answer = new ArrayList<>();
         for(int i =0;i<arr.length;i++){
-            count=0;
-            for(int j=i;j<arr.length;j++) {
-                if (arr[i]==arr[j]) {
-                        count++;
-                    }
-                }
-            if(count>minLen && !answer.contains(arr[i])){
-                answer.add(arr[i]);
+            if(counter.containsKey(arr[i])){
+                int value = counter.get(arr[i]);
+                counter.remove(arr[i]);
+                counter.put(arr[i],value+1);
+            }else{
+               counter.put(arr[i],1) ;
             }
         }
+        counter.forEach((key, value) -> {
+            if(value>minLen && !answer.contains(key)){
+                answer.add(key);
+            }
+        });
         return answer;
     }
 }
