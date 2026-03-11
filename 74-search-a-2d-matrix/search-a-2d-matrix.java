1034ed39d9ac14bd187c23a9be74a91b3c08188e
Arman Thakur <yashpalsharma9950@gmail.com>
2026-03-11 22:47:58 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (59.58%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -16,10 +16,12 @@ class Solution {
                 break;
             }
         }
-        for(int i  =0 ;i<=col_end;i++){
-            if(matrix[possible_row][i]==target){
-                return true;
-            }
+          while(col_end>=col_start){
+            int mid  = col_start + (col_end - col_start)/2;
+            int value_mid = matrix[possible_row][mid];
+            if(value_mid==target)return true;
+            else if(value_mid<target)col_start = mid+1;
+            else col_end =  mid-1;
         }
 
         return  false;
