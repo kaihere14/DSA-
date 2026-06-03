db50903576e19cc137c6eceb0085587777b2b440
Arman Thakur <yashpalsharma9950@gmail.com>
2026-06-03 19:34:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (42.46%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,61 +1,26 @@
 class Solution {
-
-     public class MergeSort {
-
-        public static void mergeSort(int[] arr, int l, int r) {
-            if (l < r) {
-                int m = l + (r - l) / 2;
-
-                mergeSort(arr, l, m);
-                mergeSort(arr, m + 1, r);
-
-                merge(arr, l, m, r);
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int finalArray[] = new int[m+n];
+        int nums1Pointer = 0;
+        int nums2Pointer = 0;
+        int counter = 0;
+        for(int i =0;i<nums1.length;i++){
+            if(nums1Pointer==m)break;
+            if(nums2Pointer==n)break;
+            if(nums1[nums1Pointer]<=nums2[nums2Pointer]){
+                finalArray[counter++]=nums1[nums1Pointer++];
+            }else{
+                finalArray[counter++]=nums2[nums2Pointer++];
             }
         }
-
-        private static void merge(int[] arr, int l, int m, int r) {
-            int n1 = m - l + 1;
-            int n2 = r - m;
-
-            int[] L = new int[n1];
-            int[] R = new int[n2];
-
-            for (int i = 0; i < n1; i++)
-                L[i] = arr[l + i];
-            for (int j = 0; j < n2; j++)
-                R[j] = arr[m + 1 + j];
-
-            int i = 0, j = 0, k = l;
-
-            while (i < n1 && j < n2) {
-                if (L[i] <= R[j]) {
-                    arr[k] = L[i];
-                    i++;
-                } else {
-                    arr[k] = R[j];
-                    j++;
-                }
-                k++;
-            }
-
-            while (i < n1) {
-                arr[k] = L[i];
-                i++;
-                k++;
-            }
-
-            while (j < n2) {
-                arr[k] = R[j];
-                j++;
-                k++;
-            }
+        while(nums2Pointer<n){
+            finalArray[counter++]=nums2[nums2Pointer++];
         }
-    }
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-       for(int i = 0 ;i<nums2.length;i++){
-           nums1[m+i]=nums2[i];
-       }
-        MergeSort.mergeSort(nums1, 0, nums1.length - 1);
-
+        while(nums1Pointer<m){
+            finalArray[counter++]=nums1[nums1Pointer++];
+        }
+        for(int i = 0 ;i<nums1.length;i++){
+            nums1[i]=finalArray[i];
+        }
     }
 }
