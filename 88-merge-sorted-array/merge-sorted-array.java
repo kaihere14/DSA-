aad00fbe91b0052e28c9dd7f0cbbd02562d4989f
Arman Thakur <yashpalsharma9950@gmail.com>
2026-06-04 05:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (42.41%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,26 +1,20 @@
 class Solution {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int finalArray[] = new int[m+n];
-        int nums1Pointer = 0;
-        int nums2Pointer = 0;
-        int counter = 0;
-        for(int i =0;i<nums1.length;i++){
-            if(nums1Pointer==m)break;
-            if(nums2Pointer==n)break;
-            if(nums1[nums1Pointer]<=nums2[nums2Pointer]){
-                finalArray[counter++]=nums1[nums1Pointer++];
-            }else{
-                finalArray[counter++]=nums2[nums2Pointer++];
-            }
+       int nums1Pointer = m-1;
+       int nums2Pointer =n-1;
+       int index = m+n-1;
+       while(nums1Pointer>=0 && nums2Pointer>=0){
+        if(nums1[nums1Pointer]>=nums2[nums2Pointer]){
+            nums1[index]=nums1[nums1Pointer];
+            index--;nums1Pointer--;
+        }else{
+            nums1[index]=nums2[nums2Pointer];
+            index--;nums2Pointer--;
         }
-        while(nums2Pointer<n){
-            finalArray[counter++]=nums2[nums2Pointer++];
-        }
-        while(nums1Pointer<m){
-            finalArray[counter++]=nums1[nums1Pointer++];
-        }
-        for(int i = 0 ;i<nums1.length;i++){
-            nums1[i]=finalArray[i];
-        }
+       }
+       while(nums2Pointer>=0){
+        nums1[index]=nums2[nums2Pointer];
+        index--;nums2Pointer--;
+       }
     }
 }
