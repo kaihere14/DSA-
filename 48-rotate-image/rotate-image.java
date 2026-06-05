13e9ad299262557a33bad2ca16234fb41fb44015
Arman Thakur <yashpalsharma9950@gmail.com>
2026-06-05 22:04:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (67.87%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,16 +1,20 @@
+
+
 class Solution {
-    public void rotate(int[][] arr) {
-        int row = arr.length;
-        int col = arr[0].length;
-        int arr2[][] = new int[row][col];
-        for(int i =row-1;i>=0;i--){
-            for(int j = 0;j<col;j++){
-                arr2[j][row-i-1]= arr[i][j];
+    public static void rotate(int[][] matrix) {
+        for(int i = 0 ; i < matrix.length;i++){
+            for(int j = i ; j<matrix[0].length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-        for(int i =0 ;i<row;i++){
-            for(int j =0;j<col;j++){
-                arr[i][j]=arr2[i][j];
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
             }
         }
     }
