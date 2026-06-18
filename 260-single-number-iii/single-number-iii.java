a8adde956056fedcfd5061bd156387e6dddeaf88
Arman Thakur <yashpalsharma9950@gmail.com>
2026-06-18 16:00:10 +0530

    Time: 1 ms (100.00%) | Memory: 47.6 MB (96.55%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,21 +1,23 @@
 class Solution {
     public static int[] singleNumber(int[] nums) {
-        int ans[] = {0,0};
-        Arrays.sort(nums);
-        int start = 0;
-        for(int i = 1 ;i<nums.length;i++){
-            if(nums[i]!=nums[i-1] && i != nums.length-1){
-                ans[start++]= nums[i-1];
+        int xor=0;
+        if(nums.length==2){
+            int ans[]={nums[0],nums[1]};
+            return ans;
+        }
+        for(int i = 0 ;i<nums.length;i++){
+            xor = xor^nums[i];
+        }
+        int right_shift = (xor^(xor-1))&xor;
+        int b1=0,b2=0;
+        for(int i = 0 ;i<nums.length;i++){
+            if((nums[i]&right_shift)>0){
+                b1 = b1^nums[i];
             }else{
-                i++;
+                b2 = b2^nums[i];
             }
         }
-        if(start==0){
-            ans[start++]=nums[nums.length-2];
-        }
-        if(start==1){
-            ans[start]=nums[nums.length-1];
-        }
+        int ans[] = {b1,b2};
         return ans;
     }
 }
