cfac1866eec11b18b6ca767fe043d7ecfc314a1f
Arman Thakur <yashpalsharma9950@gmail.com>
2026-06-20 03:09:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (58.43%) - LeetSync

--- 2083-three-divisors/three-divisors.java
+++ 2083-three-divisors/three-divisors.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean isThree(int n) {
         int count=0;
-        for(int i = 1 ; i<=n;i++){
-            if(n%i==0){
-                count++;
-            }
-        }
+        for(int i = 1 ; i<=Math.sqrt(n);i++){
+            if (n % i == 0) {  
+                count++;              
+                if (n / i != i && n % (n / i) == 0)
+                    count++;          
+                }
+            } 
         if(count==3)return true;
         return false;
     }
