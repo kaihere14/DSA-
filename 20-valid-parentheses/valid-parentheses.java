31efc0cd4db8c1fbb96de6aedd64b627d969db8a
Arman Thakur <yashpalsharma9950@gmail.com>
2026-07-28 22:46:48 +0530

    Time: 4 ms (37.20%) | Memory: 43.6 MB (15.58%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i)=='('|| s.charAt(i)=='{'|| s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.size()==0)return false;
+                if(s.charAt(i)==')' && stack.peek()=='('){
+                    stack.pop();
+                }else if(s.charAt(i)==']' && stack.peek()=='['){
+                    stack.pop();
+                }else if(s.charAt(i)=='}' && stack.peek()=='{'){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(stack.size()!=0)return false;
+       
+        return true;
+    }
+}
