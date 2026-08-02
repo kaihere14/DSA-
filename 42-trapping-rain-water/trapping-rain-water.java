aa41e6ab8fd1cdc54e9b8e4b622dda4b18cdb303
Arman Thakur <yashpalsharma9950@gmail.com>
2026-08-02 16:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (92.02%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,31 +1,31 @@
 class Solution {
     public int trap(int[] height) {
-        //calculatinf left max
-        int left_max[] = new int[
-            height.length];
-        left_max[0] = height[0];
-        //calculating right max
-        int right_max[] = new int[height.length];
-        right_max[right_max.length - 1] = height[height.length - 1];
-        int tw=0;
-        for (int i = 1; i < height.length; i++) {
-            if (left_max[i - 1] < height[i]) {
-                left_max[i] = height[i];
-            } else {
-                left_max[i] = left_max[i - 1];
+        int total_water = 0;
+        int left_max = 0;
+        int left_pointer = 0;
+
+        int right_max = height.length-1;
+        int right_pointer = height.length-1;
+
+        while(left_max<right_max){
+            if(height[left_max]<=height[right_max]){
+
+                if(height[left_max]<=height[left_pointer]){
+                    left_max = left_pointer;
+                }else{
+                    total_water += (height[left_max]-height[left_pointer]);
+                }  
+                left_pointer++;
+            }else{
+                if(height[right_max]<=height[right_pointer]){
+                    right_max = right_pointer;
+                }else{
+                    total_water += (height[right_max]-height[right_pointer]);
+                }
+                right_pointer--;
             }
         }
-        for (int k = height.length-1; k >0 ; k--) {
-            if (right_max[k] < height[k-1]) {
-                right_max[k-1] = height[k-1];
-            } else {
-                right_max[k-1]= right_max[k];
-            }
-        }
-        for (int l = 0; l < height.length; l++) {
-            int mw = Math.min(left_max[l],right_max[l]);
-            tw += (mw-height[l]);
-        }
-        return tw;
+
+        return total_water;
     }
- }
+}
