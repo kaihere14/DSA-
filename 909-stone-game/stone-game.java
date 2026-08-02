d6b06438ac023ae7b22d66c521761a27cff26e5f
Arman Thakur <yashpalsharma9950@gmail.com>
2026-08-02 09:18:18 +0530

    Time: 8 ms (18.78%) | Memory: 46.6 MB (19.10%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean stoneGame(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        
+        for (int len = 2; len <= n; len++) {
+            for (int l = 0; l + len - 1 < n; l++) {
+                int r = l + len - 1;
+                int pickLeft = nums[l] - dp[l+1][r];
+                int pickRight = nums[r] - dp[l][r-1];
+                dp[l][r] = Math.max(pickLeft, pickRight);
+            }
+        }
+        
+        return dp[0][n-1] >= 0;
+    }
+}
