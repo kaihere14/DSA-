4d5934194768404a3e7103ed3ab067b40b7f913b
Arman Thakur <yashpalsharma9950@gmail.com>
2026-08-06 04:58:18 +0530

    Time: 1484 ms (5.00%) | Memory: 200.3 MB (5.02%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+
+        int[] ans = new int[nums.length - k + 1];
+        int ansIdx = 0;
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int curr_max = Integer.MIN_VALUE;
+        int curr_index = -1;
+
+        // Build first window
+        for (int i = 0; i < k; i++) {
+            deque.addLast(nums[i]);
+
+            if (curr_max <= nums[i]) {
+                curr_max = nums[i];
+                curr_index = i + 1;
+            }
+        }
+
+        ans[ansIdx++] = curr_max;
+        curr_index--;
+
+        // Slide window
+        for (int i = k; i < nums.length; i++) {
+
+            deque.removeFirst();
+            deque.addLast(nums[i]);
+
+            if (curr_index == 0) {
+
+                curr_max = Integer.MIN_VALUE;
+                Object[] arr = deque.toArray();
+
+                for (int j = 0; j < arr.length; j++) {
+                    int item = (Integer) arr[j];
+
+                    if (curr_max <= item) {
+                        curr_max = item;
+                        curr_index = j + 1;
+                    }
+                }
+
+                ans[ansIdx++] = curr_max;
+                curr_index--;
+            }
+            else if (curr_max > nums[i]) {
+
+                ans[ansIdx++] = curr_max;
+                curr_index--;
+            }
+            else {
+
+                curr_max = nums[i];
+                curr_index = k - 1;
+                ans[ansIdx++] = curr_max;
+            }
+        }
+
+        return ans;
+    }
+}
