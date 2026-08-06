cc1cbc3eccb2f8184e1d6ca568a3cf489f927517
Arman Thakur <yashpalsharma9950@gmail.com>
2026-08-06 20:20:30 +0530

    Time: 30 ms (74.50%) | Memory: 143.5 MB (85.86%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -2,59 +2,23 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
         int[] ans = new int[nums.length - k + 1];
-        int ansIdx = 0;
+        int counter = 0;
 
         Deque<Integer> deque = new ArrayDeque<>();
 
-        int curr_max = Integer.MIN_VALUE;
-        int curr_index = -1;
-
-        
-        for (int i = 0; i < k; i++) {
-            deque.addLast(nums[i]);
-
-            if (curr_max <= nums[i]) {
-                curr_max = nums[i];
-                curr_index = i + 1;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(deque.size()!= 0 && deque.peekFirst()<=(i-k)){
+                deque.pop();
             }
-        }
 
-        ans[ansIdx++] = curr_max;
-        curr_index--;
-
-        
-        for (int i = k; i < nums.length; i++) {
-
-            deque.removeFirst();
-            deque.addLast(nums[i]);
-
-            if (curr_index == 0) {
-
-                curr_max = Integer.MIN_VALUE;
-                Object[] arr = deque.toArray();
-
-                for (int j = 0; j < arr.length; j++) {
-                    int item = (Integer) arr[j];
-
-                    if (curr_max <= item) {
-                        curr_max = item;
-                        curr_index = j + 1;
-                    }
-                }
-
-                ans[ansIdx++] = curr_max;
-                curr_index--;
+            while(deque.size()!= 0 && nums[deque.peekLast()]<=nums[i]){
+                deque.removeLast();
             }
-            else if (curr_max > nums[i]) {
 
-                ans[ansIdx++] = curr_max;
-                curr_index--;
-            }
-            else {
+            deque.addLast(i);
 
-                curr_max = nums[i];
-                curr_index = k - 1;
-                ans[ansIdx++] = curr_max;
+            if(i>=k-1){
+                ans[counter++] = nums[deque.peekFirst()];
             }
         }
 
