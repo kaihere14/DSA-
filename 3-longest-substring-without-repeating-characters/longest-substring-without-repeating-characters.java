f114b23e081205df200df4a1379519d8a1b50f60
Arman Thakur <yashpalsharma9950@gmail.com>
2026-08-09 00:27:55 +0530

    Time: 199 ms (5.05%) | Memory: 47.7 MB (14.43%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -5,12 +5,23 @@ class Solution {
         for(int i = 0 ; i < arr.length ; i++){
             arr[i] = -1;
         }
+
+        List<Character> ch = new ArrayList();
+        int unique_counter = 0;
+        for(int i = 0 ; i < s.length() ; i++ ){
+            if(!ch.contains(s.charAt(i))){
+                unique_counter++;
+                ch.add(s.charAt(i));
+            }
+        }
+
         int left_pointer = 0;
         
 
         int ans = 0;
 
         for(int i = 0 ; i < s.length() ; i++){
+
             int index = arr[s.charAt(i)];
             if(index==-1){
                 arr[s.charAt(i)]=i;
@@ -24,6 +35,7 @@ class Solution {
                 }
             }
             ans = Math.max(ans,i - left_pointer +1);
+            if(ans==unique_counter)return ans;
 
         }
 
