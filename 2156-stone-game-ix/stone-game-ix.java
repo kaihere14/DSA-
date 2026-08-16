52c4a86bb22f94e42c713c5225f33fb2db5aca26
Arman Thakur <yashpalsharma9950@gmail.com>
2026-08-16 14:07:58 +0530

    Time: 4 ms (75.68%) | Memory: 115.1 MB (16.22%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] cnt = new int[3];
+        for (int s : stones) cnt[s % 3]++;
+        if (cnt[0] % 2 == 0) return cnt[1] >= 1 && cnt[2] >= 1;
+        return Math.abs(cnt[1] - cnt[2]) > 2;
+    }
+}
